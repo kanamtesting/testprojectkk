94af23cbe949c1dac0f6302d2fb1d03068b7373b
kanamtesting <kanamtesting@gmail.com>
2025-10-21 10:33:35 -0500

    Added new line from VS

--- testfile2.py
+++ testfile2.py
@@ -1,1 +1,2 @@
-print("Creating a file in GitHub to see if it can reflect changes in VS")
+print("Creating a file in GitHub to see if it can reflect changes in VS1")
+print("Addind secong line from VS2")
